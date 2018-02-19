0c7b9c417f68e87227ef35fc67eb3533a9567260
Noah Zemlin <noah.zemlin@ou.edu>
2018-02-18 18:07:34 -0600

    Fixed Stage stuff

--- src/main/java/edu/ou/cs/hci/stages/Stage2.java
+++ src/main/java/edu/ou/cs/hci/stages/Stage2.java
@@ -1,13 +1,17 @@
 package edu.ou.cs.hci.stages;
 
+import edu.ou.cs.hci.stages.frames.*;
 import edu.ou.cs.hci.stages.panels.*;
 
-public final class Stage1
+public final class Stage2
 {
 	public static void main(String[] args)
 	{
 		//Create the Main Window at 50, 50
 		new ClubPenguin(50, 50);
+		
+		//Create the Scenario Window at 300, 150
+		new ScenarioFrame(300, 150);
 	}
 	
 }
